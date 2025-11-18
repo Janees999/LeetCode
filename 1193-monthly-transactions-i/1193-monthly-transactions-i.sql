fc4f1e6ce488d2ded2376810ad91952b721cfb8f
Janees <muhammedjanees999@gmail.com>
2025-11-18 10:48:58 +0530

    Time: 420 ms (61.72%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -5,6 +5,6 @@ SELECT
     COUNT(*) AS trans_count,
     SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
     SUM(amount) AS trans_total_amount,
-    SUM(CASE WHEN state = 'approved' THEN amount ELSE 2 END) AS approved_total_amount
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM Transactions
 GROUP BY TO_CHAR(trans_date, 'YYYY-MM'), country;
