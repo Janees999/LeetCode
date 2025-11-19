7e212ee3b6fd15a600f3b74fc1b3cb01be128e91
Janees <muhammedjanees999@gmail.com>
2025-11-19 21:19:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,16 @@
+/**
+ * @param {any} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(val2) {
+            if (val === val2) return true;
+            else throw "Not Equal";
+        },
+        notToBe: function(val2) {
+            if (val !== val2) return true;
+            else throw "Eqal";
+        }
+    };
+};
